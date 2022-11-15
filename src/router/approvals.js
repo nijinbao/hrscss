bb08f4f4a655291cfc4792b8eb9566c3b027bdbf
倪进保 <17756691102@163.com>
2022-11-15 23:23:41 +0800

    完成其他模块的页面和路由的快速搭建

---
+++ src/router/approvals.js
@@ -0,0 +1,16 @@
+import layout from "@/layout/index.vue"
+export default {
+  path:"/approvals",
+  component:layout,
+  children:[
+    {
+      // path设置为空字符串，在展示layout组件的时候会自动匹配apprpvals组件作为二级路由进行展示
+      path:"",
+      component:()=>import("@/views/approvals/index.vue"),
+      // 传递一些元信息，方便展示侧边栏的标题
+      meta:{
+        title:"审批管理"
+      }
+    }
+  ]
+}
